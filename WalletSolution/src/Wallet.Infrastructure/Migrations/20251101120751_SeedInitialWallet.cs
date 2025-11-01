4b1b536b3e28a4c4b1c60e9378569638ca35b7f7
altair <ar80pro@gmail.com>
2025-11-01 14:17:36 +0200

    data-seed initial wallet

---
+++ WalletSolution/src/Wallet.Infrastructure/Migrations/20251101120751_SeedInitialWallet.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wallet.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialWallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var userId = new Guid("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee");
+            var walletId = Guid.NewGuid();
+
+            migrationBuilder.InsertData(
+                table: "Wallets",
+                columns: new[] { "Id", "UserId", "Balance"},
+                values: new object[] { walletId, userId, 1000m }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Wallets",
+                keyColumn: "UserId",
+                keyValue: new Guid("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
+            );
+        }
+    }
+}
